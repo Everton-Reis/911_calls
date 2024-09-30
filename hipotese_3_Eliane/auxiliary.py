8c0940e4e7cd10bea3d3e59bfa4892cdfb082c7a
ElianeMoreira <elianemoreira121095@gmail.com>
2024-09-29 21:53:30 -0300

    problemas do arquivo auxiliar solucionados

--- hipotese_3_Eliane/auxiliary.py
+++ hipotese_3_Eliane/auxiliary.py
@@ -15,44 +15,43 @@ def load_data(filepath):
     -------
     pandas.DataFrame
         A DataFrame containing the 'recordId', 'district', and 'description' columns, with missing values removed.
-
-    Notes
-    -----
-    The CSV file is expected to have columns 'recordId', 'district', and 'description'.
-    The function reads the file with tab (`\t`) as the separator and selects only these columns.
     """
     
     columns_to_use = ['recordId', 'district', 'description']
-    df = pd.read_csv(filepath, usecols=columns_to_use, sep="\t") 
-    return df[['recordId', 'district', 'description']].dropna()
+    try:
+        df = pd.read_csv(filepath, usecols=columns_to_use, sep="\t") 
+        return df[['recordId', 'district', 'description']].dropna()
+    except FileNotFoundError:
+        print(f"Error: The file {filepath} was not found.")
+        return pd.DataFrame() 
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return pd.DataFrame() 
 
 
-def save_top_descriptions(df, output_csv="Top_30_Descriptions.csv"):
+def save_top_descriptions(df, output_csv='Top_50_Descriptions.csv'):
     """
-    Extracts the 30 most ferquent descriptions from the DataFrame and saves them to a CSV file.
+    Extracts the 50 most frequent descriptions from the DataFrame and saves them to a CSV file.
 
     Parameters
     ----------
     df : pandas.DataFrame
         The input DataFrame that contains the 'description' column.
     output_csv : str
-        The path where the output CSV file will be saved (defalt is 'Top_30_Descriptions.csv').
+        The path where the output CSV file will be saved (default is 'Top_50_Descriptions.csv').
 
     Returns
     -------
-    None
-
-    Notes
-    -----
-    The function selects the top 30 most frequent values from the 'description' column,
-    counts their occurrences, and saves them to a csv file with two columns:
-    'Descriptions' and 'Count'.
+    top_descriptions_df
     """
     
-    top_descriptions = df['description'].value_counts().head(30)
+    top_descriptions = df['description'].value_counts().head(50)
     top_descriptions_df = top_descriptions.reset_index()
     top_descriptions_df.columns = ['Description', 'Count']
-    top_descriptions_df.to_csv('Top_30_Descriptions.csv', index=False)
+    top_descriptions_df.to_csv(output_csv, index=False)
+
+    return top_descriptions_df
 
 df = load_data(filepath)
-save_top_descriptions(df)
+if not df.empty: 
+    save_top_descriptions(df)
